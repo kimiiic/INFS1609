d94419ac675d0486801719812879cc9c8b354f41
ryotokuro <ryotokuro@gmail.com>
2019-02-24 23:54:15 +1100

    added tutorial notes (pseudo-code)

--- assignment-1/src/assignment1.java
+++ assignment-1/src/assignment1.java
@@ -1,5 +1,21 @@
+import java.util.Scanner; // Get scanner
+
 public class assignment1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // SPEC: "accepts 6 numbers"
+        Scanner input = new Scanner(System.in);
+
+        int[] array;
+        array.a
+
+        // SPEC: "prints the top 2 numbers"
+        System.out.println(highest, second);
     }
 }
+
+// Tests
+// INPUT: 7 2 4 10 9 3
+// First highest number: 10
+// Second highest number: 9
+
