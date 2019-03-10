808fa0dd861c5bbcea4ec497ec2bc045d8660261
ryotokuro <ryotokuro@gmail.com>
2019-03-10 23:56:38 +1100

    adding comments to program

--- week-3-tutorial/src/Tut3EX1.java
+++ week-3-tutorial/src/Tut3EX1.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner class
 
+// Program determines whether a character input is a vowel or not
 public class Tut3EX1{
 
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a char: ");
-        char character = input.next().charAt(0);
+        Scanner input = new Scanner(System.in); // Define new scanner named input
 
+        System.out.print("Enter a char: "); // Prompt ask for character
+        char character = input.next().charAt(0); // Store input into 'character' variable
+
+        // If character is a vowel (a (A), e (E), i (I), o (O), u (U)
         if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
-            System.out.println("'" + character + "' is a vowel");
+            System.out.println("'" + character + "' is a vowel"); // Tell user their input is a vowel
+        // If character turns out not to be above
         } else {
             System.out.println("'" + character + "' is not a vowel");
         }
--- week-3-tutorial/src/Tut3EX2.java
+++ week-3-tutorial/src/Tut3EX2.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner class
 
+// Program determines whether an input year is a leap year or not
 public class Tut3EX2 {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // Define new scanner as 'input'
+
         System.out.print("Enter a year: ");
         int year = input.nextInt();
 
+        // If year is divisible by 400
         if ((double)year % 400 == 0) {
             System.out.println(year + " is a leap year");
-        } else if((double)year % 100 == 0) {
+        // If year is divisible by 100
+        } else if((double)year % 100 == 0) { // Then it's not a leap year
             System.out.println(year + " is not a leap year");
-        } else if((double)year % 4 == 0) {
+        // Alternatively, if it is divisible by 4
+        } else if((double)year % 4 == 0) { // Then it is a leap year
             System.out.println(year + " is a leap year");
-        }else {
+        // If none of the above are met
+        } else { // It isn't a leap year
         System.out.println(year + " is not a leap year");
     }
 }
