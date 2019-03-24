e1e6418e180c686ffa0194293710cf59a5658928
ryotokuro <ryotokuro@gmail.com>
2019-03-24 23:20:40 +1100

    added ex1

---
+++ week-5-tutorial/src/Tut5EX1.java
@@ -0,0 +1,14 @@
+public class Tut5EX1{
+
+    public static void main(String[] args){
+        printINFS();
+        printINFS();
+        printINFS();
+    }
+
+    public static void printINFS(){
+        System.out.println("INFS1609");
+        System.out.println("Fundamentals of Business Programming");
+    }
+
+}
