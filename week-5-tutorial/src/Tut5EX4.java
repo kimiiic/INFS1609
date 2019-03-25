a44ada3fe985fa06ef09aad62ae1fc98f362acf2
ryotokuro <ryotokuro@gmail.com>
2019-03-25 12:12:30 +1100

    used scanner for manual testing purposes

--- week-5-tutorial/src/Tut5EX4.java
+++ week-5-tutorial/src/Tut5EX4.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Tut5EX4 {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string of numbers: ");
+        int numbers = input.nextInt();
+
+        System.out.println("The sum of the digits in your string adds up to " + sumDigits(numbers));
     }
 
     public static int sumDigits(int digits) {
