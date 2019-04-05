a0b859ff593335f6f0ab31469f32571411fa9990
ryotokuro <ryotokuro@gmail.com>
2019-04-05 23:56:03 +1100

    added tut7 ex1 file

---
+++ week7-tutorial/src/Tut7EX1.java
@@ -0,0 +1,14 @@
+public class Tut7EX1 {
+
+    public static void main(String[] args){
+        Rectangle testRectangle = new Rectangle(1.5 , 6);
+
+        Rectangle r2 = new Rectangle(2, 2);
+        System.out.println(r2.getWidth());
+        System.out.println(r2.getArea());
+        System.out.println(r2.getPerimeter());
+
+        System.out.println(testRectangle.getWidth());
+    }
+
+}
