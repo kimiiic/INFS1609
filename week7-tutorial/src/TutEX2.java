b4cc095f7ec3e354b40298747c2ef03dcc84c0de
ryotokuro <ryotokuro@gmail.com>
2019-04-06 23:35:23 +1100

    added ex2 run file - creates object and runs tests

--- week7-tutorial/src/TutEX2.java
+++ week7-tutorial/src/TutEX2.java
@@ -1,3 +1,17 @@
-public class TutEX2 {
-    // Will push working code once the deadline has passed
+public class Tut7EX2{
+
+    public static void main(String[] args){
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        TimeConsumingTask.shortTask();
+        stopWatch.stop();
+
+        System.out.print(stopWatch.getElapsedTime());
+
+        // TEST
+        TimeConsumingTask.shortTask();
+        TimeConsumingTask.mediumTask();
+        TimeConsumingTask.longTask();
+    }
+
 }
