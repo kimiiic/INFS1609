260465a6ed6b368e49bef8913533b5c72cb28e5a
ryotokuro <ryotokuro@gmail.com>
2019-04-14 13:09:49 +1000

    added person class attributes and methods

--- week8-tutorial/src/Person.java
+++ week8-tutorial/src/Person.java
@@ -1,3 +1,58 @@
 public class Person {
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
 
+    public Person() {
+
+    }
+
+    public Person(String name, String address, String phone, String email) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    // TOSTRING
+    public String toString() {
+        return "Person," + getName() + "," + getAddress() + "," + getPhone() + "," + getEmail();
+    }
+
+    // NAME
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // ADDRESS
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // PHONE
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // EMAIL
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
