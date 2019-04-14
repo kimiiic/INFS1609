d3f8c95a6aa93194cabd71367e91f223b88a6e63
ryotokuro <ryotokuro@gmail.com>
2019-04-14 13:25:48 +1000

    added student class

--- week8-tutorial/src/Student.java
+++ week8-tutorial/src/Student.java
@@ -1,2 +1,28 @@
-public class Student {
+public class Student extends Person {
+    private String faculty;
+
+    public Student(String name, String address, String phone, String email, String faculty) {
+        super(name, address, phone, email);
+
+        this.faculty = faculty;
+    }
+
+    public Student() {
+
+    }
+
+    // TOSTRING
+    @Override
+    public String toString() {
+        return "Student," + getName() + "," + getAddress() + "," + getPhone() + "," + getEmail() + "," + getFaculty();
+    }
+
+    // FACULTY
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
 }
