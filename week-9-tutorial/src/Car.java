63890090a862e28cb87b3bcd3aa7841318430429
ryotokuro <ryotokuro@gmail.com>
2019-04-20 11:17:32 +1000

    filled in Car subclass of Vehicle

--- week-9-tutorial/src/Car.java
+++ week-9-tutorial/src/Car.java
@@ -1,2 +1,28 @@
-public class Car {
+public class Car extends Vehicle {
+    // ATTRIBUTES
+    private int setCapacity;
+
+    // CONSTRUCTORS
+    public Car() {
+
+    }
+
+    public Car(int wheels, String motor, int setCapacity) {
+        super(wheels, motor);
+        this.setCapacity = setCapacity;
+    }
+
+    // GETTERS & SETTERS
+    public int getSetCapacity() {
+        return setCapacity;
+    }
+
+    public void setSetCapacity(int setCapacity) {
+        this.setCapacity = setCapacity;
+    }
+
+    // METHODS
+    public void start() {
+        System.out.println("rumble");
+    }
 }
