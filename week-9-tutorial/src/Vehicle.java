39aa6c7bda2c6833b3e3c01cb78d949fe8c05e13
ryotokuro <ryotokuro@gmail.com>
2019-04-20 11:15:08 +1000

    added abstract class definition Vehicle

--- week-9-tutorial/src/Vehicle.java
+++ week-9-tutorial/src/Vehicle.java
@@ -1,2 +1,37 @@
-public class Vehicle {
+public abstract class Vehicle {
+    // ATTRIBUTES
+    private int wheels;
+    private String motor;
+
+    // CONSTRUCTORS
+    // 1. Default
+    public Vehicle() {
+
+    }
+
+    public Vehicle(int wheels, String motor) {
+        this.wheels = wheels;
+        this.motor = motor;
+    }
+
+    // GETTERS & SETTERS
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor() {
+        this.motor = motor;
+    }
+
+    // METHODS
+    // 1. Abstract method - returns no values
+    public abstract void start();
 }
