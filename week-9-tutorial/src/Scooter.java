2b9a161e3b94966fdff02b476f01f40b5a9e291a
ryotokuro <ryotokuro@gmail.com>
2019-04-21 19:39:34 +1000

    filled Scooter class

--- week-9-tutorial/src/Scooter.java
+++ week-9-tutorial/src/Scooter.java
@@ -1,2 +1,28 @@
-public class Scooter {
+public class Scooter extends Vehicle {
+    // ATTRIBUTES
+    private int batteryCapacity;
+
+    // CONSTRUCTORS
+    public Scooter() {
+
+    }
+
+    public Scooter(int wheels, String motor, int batteryCapacity) {
+        super(wheels, motor);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    // GETTERS & SETTERS
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity() {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    // METHODS
+    public void start() {
+        System.out.println("beep");
+    }
 }
