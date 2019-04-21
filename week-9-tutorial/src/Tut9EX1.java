3a3663750ab1024b5c63d83cd9f8d5076e21e726
ryotokuro <ryotokuro@gmail.com>
2019-04-21 19:56:48 +1000

    added main file

--- week-9-tutorial/src/Tut9EX1.java
+++ week-9-tutorial/src/Tut9EX1.java
@@ -1,2 +1,13 @@
-public class Tut9EX1 {
+public class Tut9EX1{
+
+    public static void main(String[] args){
+        // DONT WORK cause abstract
+        // Vehicle v = new Vehicle(1, "YES");
+
+        Scooter scoot = new Scooter(1, "Yay", 3);
+        scoot.start();
+        Car cr = new Car(5, "B", 3);
+        cr.start();
+    }
+
 }
